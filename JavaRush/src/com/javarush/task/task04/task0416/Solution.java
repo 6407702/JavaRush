d6f23bab9e1bcad41a40520be21986cc3a02f725
16733228 <16733228@SBTLED-SD096176.sigma.sbrf.ru>
2018-09-14 14:40:43 +0300

    task0416

--- JavaRush/src/com/javarush/task/task04/task0416/Solution.java
+++ JavaRush/src/com/javarush/task/task04/task0416/Solution.java
@@ -3,7 +3,7 @@ package com.javarush.task.task04.task0416;
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/// TODO Auto-generated method stub
 
 	}
 
