47b6a585a08192bc3541a99550bbf54d5d357399
6407702 <Dima120388>
2018-09-16 11:41:32 +0300

    task0425

--- JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
+++ JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -1,13 +1,49 @@
 package com.javarush.task.task04.task0425;
 
 /* 
-Р¦РµР»СЊ СѓСЃС‚Р°РЅРѕРІР»РµРЅР°!
+Ввести с клавиатуры два целых числа, которые будут координатами точки, не лежащей на координатных осях OX и OY.
+Вывести на экран номер координатной четверти, в которой находится данная точка.
+Подсказка:
+Принадлежность точки с координатами (a,b) к одной из четвертей определяется следующим образом:
+для первой четверти a>0 и b>0;
+для второй четверти a<0 и b>0;
+для третьей четверти a<0 и b<0;
+для четвертой четверти a>0 и b<0.
 */
 
 import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //РЅР°РїРёС€РёС‚Рµ С‚СѓС‚ РІР°С€ РєРѕРґ
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String sA = reader.readLine();
+		String sB = reader.readLine();
+		int a = Integer.parseInt(sA);
+		int b = Integer.parseInt(sB);
+
+		int result = 0;
+
+		if (a > 0 & b > 0) {
+
+			result = 1;
+		}
+
+		else if (a < 0 & b > 0) {
+			result = 2;
+
+		}
+
+		else if (a < 0 & b < 0) {
+			result = 3;
+
+		}
+
+		else if (a > 0 & b < 0) {
+			result = 4;
+
+		}
+
+		System.out.println(result);
+	}
 }
