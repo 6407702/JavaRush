076c2c50bfea0f3c9b96df41a19686704b972e95
6407702 <Dima120388>
2018-09-16 10:51:55 +0300

    task0421

--- JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
+++ JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
@@ -1,13 +1,31 @@
 package com.javarush.task.task04.task0421;
 
 /* 
-РќР°СЃС‚СЏ РёР»Рё РќР°СЃС‚СЏ?
+Настя или Настя?
+Ввести с клавиатуры два имени, и если имена одинаковые вывести сообщение "Имена идентичны".
+Если имена разные, но их длины равны - вывести сообщение - "Длины имен равны".
+Если имена и длины имен разные - ничего не выводить.
 */
 
 import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //РЅР°РїРёС€РёС‚Рµ С‚СѓС‚ РІР°С€ РєРѕРґ
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String sA1 = reader.readLine();
+		String sA2 = reader.readLine();
+
+		if (sA1.equals(sA2)) {
+			System.out.println("Имена идентичны");
+
+		}
+
+		else if (sA1.length() == sA2.length()) {
+
+			System.out.println("Длины имен равны");
+
+		}
+
+	}
 }
