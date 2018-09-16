595643225722d7f90fdf2be2253216e317b20b75
6407702 <Dima120388>
2018-09-16 11:19:14 +0300

    task0424

--- JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
+++ JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -1,13 +1,27 @@
 package com.javarush.task.task04.task0424;
 
 /* 
-РўСЂРё С‡РёСЃР»Р°
+Ввести с клавиатуры три целых числа. Одно из чисел отлично от двух других, равных между собой. 
+Вывести на экран порядковый номер числа, отличного от остальных.
 */
 
 import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //РЅР°РїРёС€РёС‚Рµ С‚СѓС‚ РІР°С€ РєРѕРґ
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int a1 = Integer.valueOf(reader.readLine());
+		int a2 = Integer.valueOf(reader.readLine());
+		int a3 = Integer.valueOf(reader.readLine());
+
+		if (a1 == a2) {
+			if (a3 != a1) {
+				System.out.println(3);
+			}
+		} else if (a2 == a3) {
+			System.out.println(1);
+		} else if (a3 == a1) {
+			System.out.println(2);
+		}
+	}
 }
