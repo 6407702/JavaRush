0a5641ad8aaf00646515a8aff0bfa50ca5f2b099
6407702 <Dima120388>
2018-09-16 14:01:31 +0300

    task0428

---
+++ JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task04.task0428;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[3];
+        arr[0] = Integer.valueOf(reader.readLine());
+        arr[1] = Integer.valueOf(reader.readLine());
+        arr[2] = Integer.valueOf(reader.readLine());
+
+        int counter = 0;
+
+        for (int a : arr) {
+            if (a >= 0) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
