908128d555038090ba6b437cd6779ac0247faecb
6407702 <Dima120388>
2018-09-16 11:19:45 +0300

    task0423

--- JavaSyntax/src/com/javarush/task/task04/task0423/Solution.java
+++ JavaSyntax/src/com/javarush/task/task04/task0423/Solution.java
@@ -1,13 +1,23 @@
 package com.javarush.task.task04.task0423;
 
 /* 
-Р¤РµР№СЃ-РєРѕРЅС‚СЂРѕР»СЊ
+Ввести с клавиатуры имя и возраст. Если возраст больше 20 вывести надпись "И 18-ти достаточно".
 */
 
 import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //РЅР°РїРёС€РёС‚Рµ С‚СѓС‚ РІР°С€ РєРѕРґ
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String sA1 = reader.readLine();
+		String sA2 = reader.readLine();
+		int a2 = Integer.parseInt(sA2);
+		
+		
+		if (a2>20) {
+			
+			System.out.println("И 18-ти достаточно");
+		}
     }
 }
