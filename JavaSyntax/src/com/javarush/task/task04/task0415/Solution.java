eaee4c6e08d65cf2a86826b95c0d34de57bdff26
6407702 <Dima120388>
2018-09-16 08:07:14 +0300

    task0417

--- JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -1,12 +1,61 @@
 package com.javarush.task.task04.task0415;
 
+/*
+Ввести с клавиатуры три числа а, b, c - стороны предполагаемого треугольника.
+Определить возможность существования треугольника по сторонам.
+Результат вывести на экран в следующем виде:
+"Треугольник существует." - если треугольник с такими сторонами существует.
+"Треугольник не существует." - если треугольник с такими сторонами не существует.
+
+Подсказка:
+Треугольник существует только тогда, когда сумма любых двух его сторон больше третьей.
+Требуется сравнить каждую сторону с суммой двух других.
+Если хотя бы в одном случае сторона окажется больше или равна сумме двух других, то треугольника с такими сторонами не существует.
+
+
+Требования:
+1. Программа должна считывать три числа c клавиатуры.
+2. Программа должна выводить текст на экран согласно условия задачи.
+3. Если треугольник с такими сторонами может существовать, необходимо вывести текст: "Треугольник существует."
+4. Если треугольник с такими сторонами не может существовать, необходимо вывести текст: "Треугольник не существует."
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		
-		
+		String sA = reader.readLine();
+		String sB = reader.readLine();
+		String sC = reader.readLine();
+		int a = Integer.parseInt(sA);
+		int b = Integer.parseInt(sB);
+		int c = Integer.parseInt(sC);
+
+		if (Compare(a, b, c) & Compare(a, c, b) & Compare(b, c, a)) {
+
+			System.out.println("Треугольник существует.");
+		}
+
+		else {
+
+			System.out.println("Треугольник не существует.");
+		}
+
+	}
+
+	public static boolean Compare(int a, int b, int c) {
+
+		if (a + b > c) {
+
+			return true;
+		}
+
+		else
+			return false;
+
 	}
 
 }
--- JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
+++ JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
@@ -1,13 +1,49 @@
 package com.javarush.task.task04.task0417;
 
-/* 
-РЎСѓС‰РµСЃС‚РІСѓРµС‚ Р»Рё РїР°СЂР°?
-*/
+/*
+ * 
+ *Существует ли пара?
+Ввести с клавиатуры три целых числа. Определить, имеется ли среди них хотя бы одна пара равных между собой чисел.
+Если такая пара существует, вывести на экран числа через пробел.
+Если все три числа равны между собой, то вывести все три.
+ */
 
 import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        //РЅР°РїРёС€РёС‚Рµ С‚СѓС‚ РІР°С€ РєРѕРґ
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String sA1 = reader.readLine();
+		String sA2 = reader.readLine();
+		String sA3 = reader.readLine();
+		int a1 = Integer.parseInt(sA1);
+		int a2 = Integer.parseInt(sA2);
+		int a3 = Integer.parseInt(sA3);
+
+		if (a1 == a2 & a2 == a3) {
+			System.out.print(a1 + " ");
+			System.out.print(a2 + " ");
+			System.out.print(a3);
+		}
+
+		else if (a1 == a2) {
+
+			System.out.println(a1 + " " + a2);
+
+		}
+
+		else if (a1 == a3) {
+
+			System.out.println(a1 + " " + a3);
+
+		}
+
+		else if (a2 == a3) {
+
+			System.out.println(a2 + " " + a3);
+
+		}
+
+	}
 }
